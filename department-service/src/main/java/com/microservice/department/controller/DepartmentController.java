b3549966bf81d3c737d4d6229f8ab5c784034240
g94leb <67076434+g94leb@users.noreply.github.com>
2022-03-02 11:34:15 -0500

    Add files via upload

---
+++ department-service/src/main/java/com/microservice/department/controller/DepartmentController.java
@@ -0,0 +1,33 @@
+package com.microservice.department.controller;
+
+import com.microservice.department.entity.Department;
+import com.microservice.department.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/departments")
+@Slf4j
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/")
+    public Department saveDepartment(@RequestBody Department department) {
+        log.info("Save Department Controller");
+        return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/{id}")
+    public Department findDepartmentById(@PathVariable("id") Long departmentId){
+        log.info("Find Department By Id Controller");
+        return departmentService.findDepartmentById(departmentId);
+    }
+
+
+}
+
+
